67c75f2e4c93b2849e5f537da122c092f005c9d4
Andrii-Verhun <113340615+Andrii-Verhun@users.noreply.github.com>
2023-02-19 10:42:46 +0200

    Task 1 complete

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const elemGallery = galleryItems.map((image) => {
+    const elemLink = document.createElement('a');
+    elemLink.classList.add('gallery__item');
+    elemLink.href = image.original;
+
+    const elemImg = document.createElement('img');
+    elemImg.classList.add('gallery__image');
+    elemImg.src = image.preview;
+    elemImg.alt = image.description;
+    elemImg.dataset.source = image.original;
+
+    elemLink.append(elemImg);
+    return elemLink;
+});
+
+gallery.append(...elemGallery);
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: '250',
+});
