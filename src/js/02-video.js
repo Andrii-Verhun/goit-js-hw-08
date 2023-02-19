de6d55a7ec080a7774102c701a8f7e65b42a60ed
Andrii-Verhun <113340615+Andrii-Verhun@users.noreply.github.com>
2023-02-19 11:58:01 +0200

    Task 2 complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+
+const player = new Player('vimeo-player', {
+    id: 236203659,
+});
+
+player.on('timeupdate', throttle((data) => {
+        localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds));
+    }, 1000));
+
+try {
+  player.setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')));
+} catch (error) {
+  console.log('Parsing error!');
+};
 
